e757bbed787a14aaa0d62f869509e9e4f1307b0e
cvk007 <chintanvkotecha@gmail.com>
2020-09-12 00:20:17 +0530

    Failed for 2 as expected is 2 but actual is 1.

--- src/main/java/Stage2Test.java
+++ src/main/java/Stage2Test.java
@@ -6,5 +6,6 @@ public class Stage2Test {
     public void test() {
         Stage2 fizzBuzz = new Stage2();
         Assert.assertEquals("1",fizzBuzz.action(1));
+        Assert.assertEquals("2",fizzBuzz.action(2));
     }
 }
