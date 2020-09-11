77df895f435d5e560cb95cf870d7e4a71aff9ee2
cvk007 <chintanvkotecha@gmail.com>
2020-09-12 00:37:49 +0530

    Changed the return to Fizz if input contains 3 and the test case passed.

--- src/main/java/Stage2.java
+++ src/main/java/Stage2.java
@@ -1,7 +1,7 @@
 public class Stage2 {
 
     public String action(int i) {
-        if (i%3 == 0)
+        if (i%3 == 0 || Integer.toString(i).contains("3"))
             return "Fizz";
         if (i%5==0)
             return "Buzz";
