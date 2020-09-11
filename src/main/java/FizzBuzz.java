96686ca9967746ebe2ab67a53457adf1863487bb
cvk007 <chintanvkotecha@gmail.com>
2020-09-11 23:42:20 +0530

    Return the argument input for passing all the current test case.

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -1,8 +1,5 @@
 public class FizzBuzz {
     public String action(int input) {
-        if (input==1)
-            return "1";
-        else
-            return "2";
+        return input + "";
     }
 }
