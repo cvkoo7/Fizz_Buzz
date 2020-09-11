d0bc0463c2a3dc1536f2f0d3d6c23b2babae92f8
cvk007 <chintanvkotecha@gmail.com>
2020-09-12 00:24:14 +0530

    Changed the return to Fizz if input is 3 and the test case passed.

--- src/main/java/Stage2.java
+++ src/main/java/Stage2.java
@@ -3,6 +3,8 @@ public class Stage2 {
     public String action(int i) {
         if (i == 2)
             return "2";
+        if (i==3)
+            return "Fizz";
         else
             return "1";
     }
