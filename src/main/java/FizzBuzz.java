db62c1cc907f8798e65e9e3a9fbf19bc1cc38ba8
cvk007 <chintanvkotecha@gmail.com>
2020-09-12 00:08:55 +0530

    In FizzBuzz class added the for loop to get the required output from 1 to 100.

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -1,5 +1,10 @@
 public class FizzBuzz {
-    public String action(int input) {
+    public static void main(String[] args) {
+        for(int i=1; i<=100;i++){
+            System.out.println(action(i));
+        }
+    }
+    public static String action(int input) {
         if (input % 3 == 0 && input % 5 == 0){
             return "FizzBuzz";
         }
