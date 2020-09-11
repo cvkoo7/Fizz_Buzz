0569a55c4c3e1cfe4ba146b8ecd6be715ebcbdf8
cvk007 <chintanvkotecha@gmail.com>
2020-09-11 23:44:50 +0530

    As in the problem we need Fizz if the number is divisible by 3 but actual is 3 so assertion failed.

--- src/main/java/FizzbuzzTest.java
+++ src/main/java/FizzbuzzTest.java
@@ -7,6 +7,7 @@ public class FizzbuzzTest {
         FizzBuzz fizzBuzz = new FizzBuzz();
         Assert.assertEquals("1",fizzBuzz.action(1));
         Assert.assertEquals("2",fizzBuzz.action(2));
+        Assert.assertEquals("Fizz",fizzBuzz.action(3));
     }
 }
 
