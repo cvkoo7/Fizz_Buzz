e189f1f46a077337185d65844152b55b4bee24d4
cvk007 <chintanvkotecha@gmail.com>
2020-09-11 23:50:03 +0530

    Failed for 5 as the expected is Buzz but actual is 5.

--- src/main/java/FizzbuzzTest.java
+++ src/main/java/FizzbuzzTest.java
@@ -8,6 +8,8 @@ public class FizzbuzzTest {
         Assert.assertEquals("1",fizzBuzz.action(1));
         Assert.assertEquals("2",fizzBuzz.action(2));
         Assert.assertEquals("Fizz",fizzBuzz.action(3));
+        Assert.assertEquals("4",fizzBuzz.action(4));
+        Assert.assertEquals("Buzz",fizzBuzz.action(5));
     }
 }
 
