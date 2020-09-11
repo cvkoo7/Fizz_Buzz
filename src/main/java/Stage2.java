b837af272fed645e7ed691f1f4e757c4ad200221
cvk007 <chintanvkotecha@gmail.com>
2020-09-12 00:32:18 +0530

    Changed the return to Buzz if input is divisible 5 and the test case passed.

--- src/main/java/Stage2.java
+++ src/main/java/Stage2.java
@@ -3,7 +3,7 @@ public class Stage2 {
     public String action(int i) {
         if (i%3 == 0)
             return "Fizz";
-        if (i==5)
+        if (i%5==0)
             return "Buzz";
         else
             return i + "";
