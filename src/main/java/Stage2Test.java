3f378e3125581c62d0beee4ca48b4d90717ac35d
cvk007 <chintanvkotecha@gmail.com>
2020-09-12 00:23:08 +0530

    Failed for 3 as expected is Fizz but actual is 1.

--- src/main/java/Stage2Test.java
+++ src/main/java/Stage2Test.java
@@ -7,5 +7,6 @@ public class Stage2Test {
         Stage2 fizzBuzz = new Stage2();
         Assert.assertEquals("1",fizzBuzz.action(1));
         Assert.assertEquals("2",fizzBuzz.action(2));
+        Assert.assertEquals("Fizz",fizzBuzz.action(3));
     }
 }
