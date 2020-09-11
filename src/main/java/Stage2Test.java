23e75eecca13a6a69a21a57aaae8dd144de29a4c
cvk007 <chintanvkotecha@gmail.com>
2020-09-12 00:16:03 +0530

    No method named action found in Stage2 error occurred.

--- src/main/java/Stage2Test.java
+++ src/main/java/Stage2Test.java
@@ -1,8 +1,10 @@
+import org.junit.Assert;
 import org.junit.Test;
 
 public class Stage2Test {
     @Test
     public void test() {
         Stage2 fizzBuzz = new Stage2();
+        String  output = fizzBuzz.action(1);
     }
 }
